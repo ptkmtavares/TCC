3de1591aeeabb6eccc114068e515264f763414d9
Patrick Tavares <ptkmoralestavares@gmail.com>
2025-01-13 18:21:49 -0300

    feat: Adicionar processamento paralelo para emails de phishing e spam e atualizar estrutura de diretórios

--- debug.py
+++
@@ -1,8 +0,0 @@
-from dataExtractor import getEmailInfo
-
-#email_info = getEmailInfo("C:\\Users\\Blast\\Documents\\TCC\\Dataset\\example\\Apresentacao_do_trabalho_final.eml")
-email_info = getEmailInfo("C:\\Users\\Blast\\Documents\\TCC\\Dataset\\example\\Aviso_de_suspensao_da_sua_conta.eml")
-
-# Iterar sobre os elementos do dicionário e imprimir cada um em uma nova linha
-for key, value in email_info.items():
-    print(f'{key}: {value}')
